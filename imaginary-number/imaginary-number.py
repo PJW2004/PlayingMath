d7ca6083c99d5ee369a329536b1970f8a10a3e44
LAPTOP-LIU8124M\hyeyun <appletreegood123@naver.com>
2022-07-03 01:48:12 +0900

    finish

--- imaginary-number/imaginary-number.py
+++ imaginary-number/imaginary-number.py
@@ -3,18 +3,23 @@ from numpy import *
 from time import *
  
 r = 1
-z = [r*exp(1j*0.0*pi)*(1.6**0.0)]
-plt.ion()
-figure, ax = plt.subplots(figsize=(8, 6))
-line1, = ax.plot(real(z), imag(z))
-plt.show()
+#z = [r*exp(1j*0.0*pi)*(1.6**0.0)]
+#plt.ion()
+#figure, ax = plt.subplots(figsize=(8,6))
+#line1, = ax.plot(real(z), imag(z))
+#plt.show()
 
-for x in linspace(0, 6, 200):
-    z.append(r*exp(1j*x*pi)*(1.6**x))
-    line1.set_xdata(real(z))
-    line1.set_ydata(imag(z))
+#for x in linspace(0, 6, 200):
+#   z.append(r*exp(1j*x*pi)*(1.6**x))
+#    line1.set_xdata(real(z))
+#    line1.set_ydata(imag(z))
 
-    figure.canvas.draw()
-    figure.canvas.flush_events()
+#    figure.canvas.draw()
+#    figure.canvas.flush_events()
     
-    sleep(0.1)
+#    sleep(0.1)
+
+Z = [r*exp(1j*x*pi)*(1.6**x) for x in linspace(0,6,200)]
+print(Z)
+plt.scatter(real(Z), imag(Z))
+plt.show()
