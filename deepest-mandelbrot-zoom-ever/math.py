9685edcb44f5bde500bac2a44141f31595db67b8
machine <appletreegood123@naver.com>
2022-07-04 21:22:20 +0900

    math

--- deepest-mandelbrot-zoom-ever/math.py
+++ deepest-mandelbrot-zoom-ever/math.py
@@ -18,10 +18,8 @@ import numpy as np
 
 np.warnings.filterwarnings("ignore")
 
-c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=21)
-members = get_members(c, num_iterations=20)
-
-plt.scatter(members.real, members.imag, color="black", marker=",", s=1)
+c = complex_matrix(-2, 0.5, -1.5, 1.5, pixel_density=512)
+plt.imshow(is_stable(c, num_iterations=20), cmap="binary")
 plt.gca().set_aspect("equal")
 plt.axis("off")
 plt.tight_layout()
